a5271c6dacac56ace34fdab0c6f3f6dd4beb44f9
hb0504511129 <hb0504511129@qq.com>
2015-01-13 18:37:40 +0800

    xml java

--- src/main/java/com/hb/models/StudentInfo.java
+++ src/main/java/com/hb/models/StudentInfo.java
@@ -2,6 +2,7 @@ package com.hb.models;
 
 import java.util.Date;
 
+import javax.inject.Named;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,46 +11,74 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.hb.util.ObjectData.DateAdapter.DateAdapter;
+
 /**
+ * 
+ * <pre>
+ * @since   ： 1.0        创建时间:    2015-1-13  上午17:52:20
+ * @author  ： huangbing   <a href="mailto:hb0504511129@126.com">发送邮件</a>
+ * @version ： 1.1
+ * 
+ * 参考资料：<a>http://my.oschina.net/zhaoqian/blog/179334</a>
  *
- *  User.java   
- *
- *  @version ： 1.1
- *  
- *  @author  ： huangbing   <a href="mailto:hb0504511129@126.com">发送邮件</a>
- *    
- *  @since   ： 1.0        创建时间:    2013-4-13  上午10:42:20
- *     
- *  TODO     : 
- *
- */
+ * @Table，@Column，@NotNull，@Pattern ,为引入jpa的实现包.hibernate-validator随便.做验证用的.
+ * 
+ * 下面为JAXB也提供了将XML实例文档反向生成Java对象树的方法，并能将Java对象树的内容重新写到XML实例文档.
+ * JAXB 2.0是JDK 1.6的组成部分。JAXB 2.2.3是JDK 1.7的组成部分。
+ * @XmlAccessorType(XmlAccessType.FIELD)指定映射本类的所有字段
+ * @XmlRootElement 用在class类的注解，常与@XmlRootElement，@XmlAccessorType一起使用.也可以单独使用,如果单独使用,需要在get方法上加@XmlElement等注解.
+ * @XmlType,在使用@XmlType的propOrder 属性时，必须列出JavaBean对象中的所有XmlElement，否则会报错。
+ * </pre>
+ */	
+@Named
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "StudentInfo")
+@XmlType(name = "StudentInfo", propOrder = { "name","password","email","sex","age","birthday","address","createTime","modifyTime"})
 @Table(name = "studentinfo")
 public class StudentInfo {
     
+	/**
+	  *  其实@XmlType已经默认会读取下面的name和age.@XmlElement在@XmlType存在的情况下,只会起到一个标识作用.
+	  */ 
+    @XmlAttribute
     @Id
     @Column(name = "Id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
+    @XmlElement
     @NotNull(message="用户名不能为空")
     private String name;
     
+    @XmlElement
     @NotNull(message="密码不能为空")
     @Size(min=4,max=10,message="密码长度必须在4-10的长度")
     private String password;
     
+    @XmlElement
     @Pattern(regexp="^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+.[a-zA-Z]{2,5}?((.cn)|(.jp))?$", message="邮箱格式不正确")
     private String email;
     
+    @XmlElement
     @Pattern(regexp="^(1|0)$", message="1表示男，0表示女")
     private int sex;            //1表示男 0表示女
     
     /**
      * 年龄
      */
+    @XmlElement
     @Pattern(regexp="^([1-9][0-9]{0,2})$", message="年龄填写不正确")
     private int age;
     
@@ -57,18 +86,23 @@ public class StudentInfo {
 	 *  生日
 	 * 基于注解的格式化
 	 */
+    @XmlElement
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date birthday;
     
     /**
      * 家庭住址
      */
+    @XmlElement
     private String address;
     
 	/**
 	 *  创建时间
 	 * 基于注解的格式化
 	 */
+    @XmlElement(name = "TransactionTime", required = true)
+    @XmlSchemaType(name = "dateTime")
+    @XmlJavaTypeAdapter(value = DateAdapter.class)
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
     
@@ -76,6 +110,7 @@ public class StudentInfo {
 	 *  修改时间
 	 * 基于注解的格式化
 	 */
+    @XmlElement
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date modifyTime;
     
---
+++ src/main/java/com/hb/util/ObjectData/JAXBCache.java
@@ -0,0 +1,34 @@
+package com.hb.util.ObjectData;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+ 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+ 
+public final class JAXBCache {
+ 
+    private static final JAXBCache instance = new JAXBCache();
+ 
+    private final ConcurrentMap<String, JAXBContext> contextCache = new ConcurrentHashMap<String, JAXBContext>();
+ 
+    private JAXBCache() {
+ 
+    }
+ 
+    public static JAXBCache instance() {
+ 
+        return instance;
+    }
+ 
+    JAXBContext getJAXBContext(Class<?> clazz) throws JAXBException {
+ 
+        JAXBContext context = contextCache.get(clazz.getName());
+        if ( context == null )
+        {
+            context = JAXBContext.newInstance(clazz);
+            contextCache.putIfAbsent(clazz.getName(), context);
+        }
+        return context;
+    }
+}
